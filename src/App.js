7a5c8e3aa880ed24372c2dae5c906036f6608e6c
fadyshaker <fshaker@ictcube.com>
2021-07-08 03:16:23 +0200

    import react-router-dom - implement routing - create new components search and list contacts

--- src/App.js
+++ src/App.js
@@ -1,56 +1,27 @@
-import React from 'react'
-import BookListContent from './components/book-list-content'
+import React from 'react';
+import { Route } from 'react-router-dom';
+import BooksList from './components/list-books';
+import SearchBooks from './components/search-books';
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
 
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <BookListContent />
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
+        <Route
+          exact path='/'
+          render={() => (
+            <BooksList />
+          )}
+        />
+        <Route
+          path='/search'
+          render={() => (
+            <SearchBooks />
+          )}
+        />
       </div>
     )
   }
---
+++ src/components/list-books.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import BookListContent from './book-list-content';
+
+class BooksList extends Component {
+    render() {
+        return (
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <BookListContent />
+                </div>
+                <div className="open-search">
+                    <Link
+                        to='/search'>
+                        Add a book
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BooksList;
